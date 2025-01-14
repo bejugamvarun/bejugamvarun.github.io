0abacc98de887e551030f35c360b2986e7035190
bejugamvarun <bejugamvarun@gmail.com>
2025-01-13 21:58:51 -0500

    added initial github workflow to deploy static site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/bejugamvarun.github.io',
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
